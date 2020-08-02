e6ebc7f31ef9b6fb3ad9f2478eb2a17e0944b5c0
Gaurav Sahni <gauravs@blinds.com>
2020-08-01 22:11:20 -0500

    Outbox - Idempotency Demo

---
+++ NsbDistTrans/Sales/DuplicateEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Messages.Events;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace Sales
+{
+    class DuplicateEventHandler : IHandleMessages<DuplicatesToPrevent>
+    {
+        static ILog log = LogManager.GetLogger<DuplicateEventHandler>();
+
+        public Task Handle(DuplicatesToPrevent message, IMessageHandlerContext context)
+        {
+            log.Info($"Received DuplicatesToPrevent, EventId = {context.MessageId}, should not be more than 1");
+            return Task.CompletedTask;
+        }
+    }
+}
